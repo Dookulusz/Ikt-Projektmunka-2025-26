21e6fd9d589400eaa4632018d47c442d7511d9f2
Dookusz <daniel.doka@umszki.hu>
2025-12-04 19:55:46 +0100

    Otthon csináltam

---
+++ Dookusz/Alkatrészek/alkatrészek.js
@@ -0,0 +1,674 @@
+var alkatrészek = {
+
+    //AMD PROCESSZOROK: 
+    ryzen99950x3d: {
+        kep: '',
+        nev: 'Ryzen 9 9950X3D',
+        ar: ''
+    },
+
+    ryzen99950x: {
+        kep: '',
+        nev: 'Ryzen 9 9950X3D',
+        ar: ''
+    },
+
+    ryzen99900X3D: {
+        kep: '',
+        nev: 'Ryzen 9 9900X3D',
+        ar: ''
+    },
+
+    ryen99900x: {
+        kep: '',
+        nev: 'Ryzen 9 9900X',
+        ar: ''
+    },
+
+    ryzen97950x: {
+        kep: '',
+        nev: 'Ryzen 9 7950X',
+        ar: ''
+    },
+
+    ryzen97900x: {
+        kep: '',
+        nev: 'Ryzen 9 7900X',
+        ar: ''
+    },
+
+    ryzen97900: {
+        kep: '',
+        nev: 'Ryzen 9 7900',
+        ar: ''
+    },
+
+    ryzen97900x3d: {
+        kep: '',
+        nev: 'Ryzen 9 7900X3D',
+        ar: ''
+    },
+
+    ryzen79800x3d: {
+        kep: '',
+        nev: 'Ryzen 7 9800X3D',
+        ar: ''
+    },
+
+    ryzen79700x: {
+        kep: '',
+        nev: 'Ryzen 7 9700X',
+        ar: ''
+    },
+
+    ryzen77800x3d: {
+        kep: '',
+        nev: 'Ryzen 7 7800X3D',
+        ar: ''
+    },
+
+    ryzen77700x: {
+        kep: '',
+        nev: 'Ryzen 7 7700X',
+        ar: ''
+    },
+
+    ryzen77700: {
+        kep: '',
+        nev: 'Ryzen 7 7700',
+        ar: ''
+    },
+
+    ryzen59600x: {
+        kep: '',
+        nev: 'Ryzen 5 9600X',
+        ar: ''
+    },
+
+    ryzen57600x: {
+        kep: '',
+        nev: 'Ryzen 5 7600X',
+        ar: ''
+    },
+
+    ryzen57600: {
+        kep: '',
+        nev: 'Ryzen 5 7600',//best
+        ar: ''
+    },
+
+    ryzen57500f: {
+        kep: '',
+        nev: 'Ryzen 5 7500F',
+        ar: ''
+    },
+
+    ryzen57400: {
+        kep: '',
+        nev: 'Ryzen 5 7400',
+        ar: ''
+    },
+
+    ryzen37300: {
+        kep: '',
+        nev: 'Ryzen 3 7300',
+        ar: ''
+    },
+
+    ryzen37100: {
+        kep: '',
+        nev: 'Ryzen 3 7100',
+        ar: ''
+    },
+
+    ryzen95950X: {
+        kep: '',
+        nev: 'Ryzen 9500X',
+        ar: ''
+    },
+
+    ryzen95900x: {
+        kep: '',
+        nev: 'Ryzen 9 5900X',
+        ar: ''
+    },
+
+    ryzen75800x3d: {
+        kep: '',
+        nev: 'Ryzen 7 5800X3D',
+        ar: ''
+    },
+
+    ryzen75800x: {
+        kep: '',
+        nev: 'Ryzen 7 5800X',
+        ar: ''
+    },
+
+    ryzen75700X: {
+        kep: '',
+        nev: 'Ryzen 7 5700X',
+        ar: ''
+    },
+
+    ryzen75700g: {
+        kep: '',
+        nev: 'Ryzen 7 5700G',
+        ar: ''
+    },
+
+    ryzen55600X: {
+        kep: '',
+        nev: 'Ryzen 5 5600X',
+        ar: ''
+    },
+
+    ryzen55600g: {
+        kep: '',
+        nev: 'Ryzen 5 5600G',
+        ar: ''
+    },
+
+    ryzen55600: {
+        kep: '',
+        nev: 'Ryzen 5 5600',
+        ar: ''
+    },
+
+    ryzen55500: {
+        kep: '',
+        nev: 'Ryzen 5 5500',
+        ar: ''
+    },
+
+    ryzen54500: {
+        kep: '',
+        nev: 'Ryzen 5 4500',
+        ar: ''
+    },
+
+    ryzen53400G: {
+        kep: '',
+        nev: 'Ryzen 5 3400G ',
+        ar: ''
+    },
+
+    ryzen53600: {
+        kep: '',
+        nev: 'Ryzen 5 3600',
+        ar: ''
+    },
+
+    ryzen51600x: {
+        kep: '',
+        nev: 'Ryzen 5 1600X',
+        ar: ''
+    },
+
+    ryzen51600: {
+        kep: '',
+        nev: 'Ryzen 5 1600',
+        ar: ''
+    },
+    ryzen51500x: {
+        kep: '',
+        nev: 'Ryzen 5 1500X',
+        ar: ''
+    },
+
+    ryzen51400: {
+        kep: '',
+        nev: 'Ryzen 5 1400',
+        ar: ''
+    },
+
+    ryzen31300x: {
+        kep: '',
+        nev: 'Ryzen 3 1300X',
+        ar: ''
+    },
+
+    ryzen31200: {
+        kep: '',
+        nev: 'Ryzen 3 1200',
+        ar: ''
+    },
+
+    ryzen71800x: {
+        kep: '',
+        nev: 'Ryzen 7 1800X',
+        ar: ''
+    },
+
+    ryzen71700x: {
+        kep: '',
+        nev: 'Ryzen 7 1700X',
+        ar: ''
+    },
+
+    ryzen71700: {
+        kep: '',
+        nev: 'Ryzen 7 1700',
+        ar: ''
+    },
+
+    ryzen33100: {
+        kep: '',
+        nev: 'Ryzen 3 3100',
+        ar: ''
+    },
+
+    ryzen33300x: {
+        kep: '',
+        nev: 'Ryzen 3 3300X',
+        ar: ''
+    },
+
+    ryzen34350G: {
+        kep: '',
+        nev: 'Ryzen 3 3450G',
+        ar: ''
+    },
+    //AMD ÉGE
+
+    //INTEL
+    inteli914900k: {
+        kep: '',
+        nev: 'Intel Core i9-14900K',
+        ar: ''
+    },
+
+    inteli914900kf: {
+        kep: '',
+        nev: 'Intel Core i9-14900KF',
+        ar: ''
+    },
+
+    inteli914900ks: {
+        kep: '',
+        nev: 'Intel Core i9-14900KS',
+        ar: ''
+    },
+
+    inteli714700k: {
+        kep: '',
+        nev: 'Intel Core i7-14700K',
+        ar: ''
+    },
+
+    inteli714700kf: {
+        kep: '',
+        nev: 'Intel Core i7-14700KF',
+        ar: ''
+    },
+
+    inteli514600k: {
+        kep: '',
+        nev: 'Intel Core i5-14600K',
+        ar: ''
+    },
+
+    inteli514600kf: {
+        kep: '',
+        nev: 'Intel Core i5-14600KF',
+        ar: ''
+    },
+
+    inteli514500: {
+        kep: '',
+        nev: 'Intel Core i5-14500',
+        ar: ''
+    },
+
+    inteli514400f: {
+        kep: '',
+        nev: 'Intel Core i5-14400F',
+        ar: ''
+    },
+
+    inteli514400: {
+        kep: '',
+        nev: 'Intel Core i5-14400',
+        ar: ''
+    },
+
+    inteli314100f: {
+        kep: '',
+        nev: 'Intel Core i3-14100F',
+        ar: ''
+    },
+
+    inteli314100: {
+        kep: '',
+        nev: 'Intel Core i3-14100',
+        ar: ''
+    },
+
+    inteli913900k: {
+        kep: '',
+        nev: 'Intel Core i9-13900K',
+        ar: ''
+    },
+
+    inteli913900kf: {
+        kep: '',
+        nev: 'Intel Core i9-13900KF',
+        ar: ''
+    },
+
+    inteli913900ks: {
+        kep: '',
+        nev: 'Intel Core i9-13900KS',
+        ar: ''
+    },
+
+    inteli713700k: {
+        kep: '',
+        nev: 'Intel Core i7-13700K',
+        ar: ''
+    },
+
+    inteli713700kf: {
+        kep: '',
+        nev: 'Intel Core i7-13700KF',
+        ar: ''
+    },
+
+    inteli513600k: {
+        kep: '',
+        nev: 'Intel Core i5-13600K',
+        ar: ''
+    },
+
+    inteli513600kf: {
+        kep: '',
+        nev: 'Intel Core i5-13600KF',
+        ar: ''
+    },
+
+    inteli513400: {
+        kep: '',
+        nev: 'Intel Core i5-13400',
+        ar: ''
+    },
+
+    inteli513400f: {
+        kep: '',
+        nev: 'Intel Core i5-13400F',
+        ar: ''
+    },
+
+    inteli313100: {
+        kep: '',
+        nev: 'Intel Core i3-13100',
+        ar: ''
+    },
+
+    inteli313100f: {
+        kep: '',
+        nev: 'Intel Core i3-13100F',
+        ar: ''
+    },
+
+    inteli912900k: {
+        kep: '',
+        nev: 'Intel Core i9-12900K',
+        ar: ''
+    },
+
+    inteli912900kf: {
+        kep: '',
+        nev: 'Intel Core i9-12900KF',
+        ar: ''
+    },
+
+    inteli712700k: {
+        kep: '',
+        nev: 'Intel Core i7-12700K',
+        ar: ''
+    },
+
+    inteli712700kf: {
+        kep: '',
+        nev: 'Intel Core i7-12700KF',
+        ar: ''
+    },
+
+    inteli512600k: {
+        kep: '',
+        nev: 'Intel Core i5-12600K',
+        ar: ''
+    },
+
+    inteli512600kf: {
+        kep: '',
+        nev: 'Intel Core i5-12600KF',
+        ar: ''
+    },
+
+    inteli512400: {
+        kep: '',
+        nev: 'Intel Core i5-12400',
+        ar: ''
+    },
+
+    inteli512400f: {
+        kep: '',
+        nev: 'Intel Core i5-12400F',
+        ar: ''
+    },
+
+    inteli312100: {
+        kep: '',
+        nev: 'Intel Core i3-12100',
+        ar: ''
+    },
+
+    inteli312100f: {
+        kep: '',
+        nev: 'Intel Core i3-12100F',
+        ar: ''
+    },
+
+    inteli911900k: {
+        kep: '',
+        nev: 'Intel Core i9-11900K',
+        ar: ''
+    },
+
+    inteli711700k: {
+        kep: '',
+        nev: 'Intel Core i7-11700K',
+        ar: ''
+    },
+
+    inteli511600k: {
+        kep: '',
+        nev: 'Intel Core i5-11600K',
+        ar: ''
+    },
+
+    inteli511400: {
+        kep: '',
+        nev: 'Intel Core i5-11400',
+        ar: ''
+    },
+
+    inteli511400f: {
+        kep: '',
+        nev: 'Intel Core i5-11400F',
+        ar: ''
+    },
+
+    inteli310100: {
+        kep: '',
+        nev: 'Intel Core i3-10100',
+        ar: ''
+    },
+
+    inteli310100f: {
+        kep: '',
+        nev: 'Intel Core i3-10100F',
+        ar: ''
+    },
+
+    inteli910900k: {
+        kep: '',
+        nev: 'Intel Core i9-10900K',
+        ar: ''
+    },
+
+    inteli710700k: {
+        kep: '',
+        nev: 'Intel Core i7-10700K',
+        ar: ''
+    },
+
+    inteli510600k: {
+        kep: '',
+        nev: 'Intel Core i5-10600K',
+        ar: ''
+    },
+
+    inteli510400: {
+        kep: '',
+        nev: 'Intel Core i5-10400',
+        ar: ''
+    },
+
+    inteli510400f: {
+        kep: '',
+        nev: 'Intel Core i5-10400F',
+        ar: ''
+    },
+
+    inteli39100f: {
+        kep: '',
+        nev: 'Intel Core i3-9100F',
+        ar: ''
+    },
+
+    inteli38100: {
+        kep: '',
+        nev: 'Intel Core i3-8100',
+        ar: ''
+    }
+
+};
+
+var procinevek = [
+    "ryzen99950x3d",
+    "ryzen99950x",
+    "ryzen99900X3D",
+    "ryen99900x",
+    "ryzen97950x",
+    "ryzen97900x",
+    "ryzen97900",
+    "ryzen97900x3d",
+    "ryzen79800x3d",
+    "ryzen79700x",
+    "ryzen77800x3d",
+    "ryzen77700x",
+    "ryzen77700",
+    "ryzen59600x",
+    "ryzen57600x",
+    "ryzen57600",
+    "ryzen57500f",
+    "ryzen57400",
+    "ryzen37300",
+    "ryzen37100",
+    "ryzen95950X",
+    "ryzen95900x",
+    "ryzen75800x3d",
+    "ryzen75800x",
+    "ryzen75700X",
+    "ryzen75700g",
+    "ryzen55600X",
+    "ryzen55600g",
+    "ryzen55600",
+    "ryzen55500",
+    "ryzen54500",
+    "ryzen53400G",
+    "ryzen53600",
+    "ryzen51600x",
+    "ryzen51600",
+    "ryzen51500x",
+    "ryzen51400",
+    "ryzen31300x",
+    "ryzen31200",
+    "ryzen71800x",
+    "ryzen71700x",
+    "ryzen71700",
+    "ryzen33100",
+    "ryzen33300x",
+    "ryzen34350G",
+    "inteli914900k",
+    "inteli914900kf",
+    "inteli914900ks",
+    "inteli714700k",
+    "inteli714700kf",
+    "inteli514600k",
+    "inteli514600kf",
+    "inteli514500",
+    "inteli514400f",
+    "inteli514400",
+    "inteli314100f",
+    "inteli314100",
+    "inteli913900k",
+    "inteli913900kf",
+    "inteli913900ks",
+    "inteli713700k",
+    "inteli713700kf",
+    "inteli513600k",
+    "inteli513600kf",
+    "inteli513400",
+    "inteli513400f",
+    "inteli313100",
+    "inteli313100f",
+    "inteli912900k",
+    "inteli912900kf",
+    "inteli712700k",
+    "inteli712700kf",
+    "inteli512600k",
+    "inteli512600kf",
+    "inteli512400",
+    "inteli512400f",
+    "inteli312100",
+    "inteli312100f",
+    "inteli911900k",
+    "inteli711700k",
+    "inteli511600k",
+    "inteli511400",
+    "inteli511400f",
+    "inteli310100",
+    "inteli310100f",
+    "inteli910900k",
+    "inteli710700k",
+    "inteli510600k",
+    "inteli510400",
+    "inteli510400f",
+    "inteli39100f",
+    "inteli38100"
+];
+
+
+
+function proci() {
+
+for (var i = 0; i < procinevek.length; i++)
+     {
+    var procinev = procinevek[i];
+    var pocok = alkatrészek[procinev];
+
+    var index = i + 1;
+
+    var felkepellek = document.getElementById('kep' + index);
+    var nevetgelek = document.getElementById('nev' + index);
+    var aronaluladok = document.getElementById('ar' + index);
+
+    if (felkepellek) felkepellek.src = pocok.kep;
+    if (nevetgelek) nevetgelek.innerHTML = pocok.nev;
+    if (aronaluladok) aronaluladok.innerHTML = pocok.ar;
+}
+
+}
