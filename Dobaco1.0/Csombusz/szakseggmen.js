c86427b02beb3d10d33559ef8ab7aa1536b8ca41
Csombork <levente.csombor.szlany@umszki.hu>
2026-01-15 21:50:18 +0100

    KÉSZ!

--- Dobaco1.0/Csombusz/szakseggmen.js
+++ Dobaco1.0/Csombusz/szakseggmen.js
@@ -2,10 +2,24 @@ var kérdés = "Van ";
 var hozzáad = "még ";
 var kérbef = " kérdésed?";
 var kiír = kérdés;
+var hanyszor = 0;
+
 
 function Beszót(){
     kiír += hozzáad;
-    document.getElementById("kerdes").innerText = kiír + kérbef;
-    document.getElementById("segítség").value = "";
-    alert("Köszönjük a visszajelzését!\nA véleménye fontos számunkra.");
+    hanyszor++;
+    if (hanyszor < 10) {
+        document.getElementById("kerdes").innerText = kiír + kérbef;
+        document.getElementById("segítség").value = "";
+        alert("Köszönjük a visszajelzését!\nA véleménye fontos számunkra.");
+    }
+    else if (hanyszor < 15) {
+        document.getElementById("kerdes").innerText = "Mi bajod van ember?!?";
+        document.getElementById("segítség").value = "";
+    }
+    else {
+        document.getElementById("kerdes").innerText = "Kész! Betelt a pohár!\n :( \nElveszem az idegesítő kis játékodat!\nTényleg idáig kellett jutni?!?";
+        document.getElementById("segítség").style.display = "none";
+        document.getElementById("kuld").style.display = "none";
+    }
 }
